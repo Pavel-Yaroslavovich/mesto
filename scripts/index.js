e411e3ab86e136a570d346381826fdccfa79a7a6
Pavel Yaroslavovich <pavel.yaroslavovich22@gmail.com>
2021-09-18 21:12:38 +0500

    Проект 4

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,33 @@
 const popup = document.querySelector('.popup');
-const popupOpenBtn = document.querySelector('.profile__rectangle');
+const popupOpenBtn = document.querySelector('.profile__edit-button');
 const popupCloseBtn = popup.querySelector('.popup__close');
 
-function popupToggle() {
-  popup.classList.toggle('popup__opened');
-}
-
-popupOpenBtn.addEventListener('click', popupToggle);
-popupCloseBtn.addEventListener('click', popupToggle);
-
+const formEl = document.querySelector('.profile__info');
+const profileName = formEl.querySelector('.profile__name');
+const profileJob = formEl.querySelector('.profile__researcher');
 
 let formElement = document.querySelector('.popup__form');
-let nameInput = formElement.querySelector('.popup__name');
-let jobInput = formElement.querySelector('.popup__job');
+let nameInput = formElement.querySelector('.popup__input_author_name');
+let jobInput = formElement.querySelector('.popup__input_author_job');
 
-function formSubmitHandler (evt) {
-  evt.preventDefault();
-
-  document.querySelector('.profile__name').textContent = nameInput.value;
-  document.querySelector('.profile__researcher').textContent = jobInput.value;
-  popupToggle();
+function openPopup() {
+  popup.classList.add('popup_opened');
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
 }
 
+function popupClose() {
+  popup.classList.remove('popup_opened');
+}
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  popupClose();
+}
+
+popupOpenBtn.addEventListener('click', openPopup);
+popupCloseBtn.addEventListener('click', popupClose);
 formElement.addEventListener('submit', formSubmitHandler);
 
