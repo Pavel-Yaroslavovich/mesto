35f004598e07f72499bb342ca1620b6fe4e1a677
Pavel Yaroslavovich <pavel.yaroslavovich22@gmail.com>
2021-09-19 15:51:47 +0500

    Проект 4

--- scripts/index.js
+++ scripts/index.js
@@ -2,9 +2,9 @@ const popup = document.querySelector('.popup');
 const popupOpenBtn = document.querySelector('.profile__edit-button');
 const popupCloseBtn = popup.querySelector('.popup__close');
 
-const formEl = document.querySelector('.profile__info');
-const profileName = formEl.querySelector('.profile__name');
-const profileJob = formEl.querySelector('.profile__researcher');
+const profileInfo = document.querySelector('.profile__info');
+const profileName = profileInfo.querySelector('.profile__name');
+const profileJob = profileInfo.querySelector('.profile__researcher');
 
 let formElement = document.querySelector('.popup__form');
 let nameInput = formElement.querySelector('.popup__input_author_name');
