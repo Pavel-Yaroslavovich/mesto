888bf86c854901a74b438c649312c0d6a3c1da9e
Pavel Yaroslavovich <pavel.yaroslavovich22@gmail.com>
2021-10-10 20:04:58 +0500

    Проект 6

--- scripts/index.js
+++ scripts/index.js
@@ -107,6 +107,7 @@ function createCard(elementText) {
   return newCard;
 }
 
+
 function renderElement(elementText) {
   const card = createCard(elementText);
   elementLists.prepend(card);
@@ -152,3 +153,90 @@ function closePopupPicture() {
 }
 
 popupClosePicture.addEventListener("click", closePopupPicture);
+
+/*==========================================Proekt 6=================================================== */
+
+const showError = (errorElement, inputElement, config) => {
+  errorElement.textContent = inputElement.validationMessage;
+  inputElement.classList.add(config.inputErrorClass);
+};
+
+const hideError = (errorElement, inputElement, config) => {
+  errorElement.textContent = inputElement.validationMessage;
+  inputElement.classList.remove(config.inputErrorClass);
+};
+
+const checkInputValidity = (formElement, inputElement, config) => {
+  const isInputNotValid = !inputElement.validity.valid;
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+
+  if (isInputNotValid) {
+    showError(errorElement, inputElement, config);
+  } else {
+    hideError(errorElement, inputElement, config);
+  }
+};
+
+const toggleButtonState = (button, isActive, config) => {
+  if (isActive) {
+    button.classList.remove(config.inactiveButtonClass);
+    button.disabled = false;
+  } else {
+    button.classList.add(config.inactiveButtonClass);
+    button.disabled = "disabled";
+  }
+};
+
+const setEventListers = (formElement, config) => {
+  const inputsList = formElement.querySelectorAll(config.inputSelector);
+  const submitButton = formElement.querySelector(config.submitButtonSelector);
+  Array.from(inputsList).forEach((inputElement) => {
+    inputElement.addEventListener("input", () => {
+      const isFormValid = formElement.checkValidity();
+      checkInputValidity(formElement, inputElement, config);
+      toggleButtonState(submitButton, isFormValid, config);
+    });
+  });
+
+  formElement.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+  });
+};
+
+const enableValidation = (config) => {
+  const forms = document.querySelectorAll(config.formSelector);
+  Array.from(forms).forEach((formElement) => {
+    setEventListers(formElement, config);
+  });
+};
+
+const validationConfig = {
+  formSelector: ".popup__form",
+  inputSelector: ".popup__input",
+  submitButtonSelector: ".popup__submit",
+  inactiveButtonClass: "popup__submit_disabled",
+  inputErrorClass: "popup__input_type_error",
+};
+
+enableValidation(validationConfig);
+
+/*------------------------------------ закрытия popup Esc, и вне popup */
+
+document.addEventListener('keydown', function (e) {
+  const key = e.key;
+  if (key === "Escape") {
+    closeProfilePopup();
+    closePopupPicture();
+    newClosePopup();
+  }
+});
+
+document.addEventListener('click', function (e) {
+  if (e.target.classList.contains('popup')) {
+    closeProfilePopup();
+    closePopupPicture();
+    newClosePopup();
+  }
+});
+
+
